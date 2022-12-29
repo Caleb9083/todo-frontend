a77d236038935a37afff06f7d4a18dc0164f0314
Caleb9083 <calebosam12@gmail.com>
2022-12-29 13:31:22 +0000

    Create starter testimonies component and intergrate in Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
+import Testimonies from "../../components/Testimonies/Testimonies";
 
 const Home = () => {
   return (
     <div>
       <div className="Home">
         <Hero />
+        <Testimonies />
       </div>
     </div>
   );
