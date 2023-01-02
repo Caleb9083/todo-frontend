66680dd0071ece352c92d9c2afbe5e94645fedea
Caleb9083 <calebosam12@gmail.com>
2023-01-02 01:59:11 +0000

    Add header and a link to All tasks section in dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -171,7 +171,18 @@ function DashboardContent() {
               {/* All Tasks */}
               <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                  <Typography
+                    component="h2"
+                    variant="h6"
+                    color="primary"
+                    gutterBottom
+                  >
+                    All Tasks
+                  </Typography>
                   {/* All Tasks /> */}
+                  <Link color="primary" href="#" sx={{ mt: 3 }}>
+                    See more tasks
+                  </Link>
                 </Paper>
               </Grid>
             </Grid>
