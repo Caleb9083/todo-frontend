c2568419a9ce1b01662a849b09654e3d056d8c0c
Caleb9083 <calebosam12@gmail.com>
2023-01-02 11:30:02 +0000

    Create boiler for My Day in Dashboard

---
+++ src/pages/Dashboard/DashboardMyDay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardWrapper from "./DashboardWrapper";
+
+const DashboardMyDay = () => {
+  return (
+    <DashboardWrapper>
+      <div>Dashboard My Day</div>
+    </DashboardWrapper>
+  );
+};
+
+export default DashboardMyDay;
