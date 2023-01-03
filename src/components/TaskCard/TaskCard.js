94c409d11a3dbd4bdbb46811891b405d07a43008
Caleb9083 <calebosam12@gmail.com>
2023-01-03 14:23:48 +0000

    Remove unused components imported in task card

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
