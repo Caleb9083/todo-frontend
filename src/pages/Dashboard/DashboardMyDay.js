1eb131a85670b6ade54a3130ff9101474d309db7
Caleb9083 <calebosam12@gmail.com>
2023-01-03 15:01:27 +0000

    Create My Day dashboard page

--- src/pages/Dashboard/DashboardMyDay.js
+++ src/pages/Dashboard/DashboardMyDay.js
@@ -1,10 +1,18 @@
 import React from "react";
+import DashboardCategory from "../../components/DashboardCategory/DashboardCategory";
 import DashboardWrapper from "./DashboardWrapper";
 
 const DashboardMyDay = () => {
   return (
     <DashboardWrapper>
-      <div>Dashboard My Day</div>
+      <DashboardCategory
+        category="My Day"
+        categoryDescription="Tasks for today"
+        todos={[
+          { name: "Learn Python" },
+          { name: "Learn Django", completed: true, important: true },
+        ]}
+      />
     </DashboardWrapper>
   );
 };
