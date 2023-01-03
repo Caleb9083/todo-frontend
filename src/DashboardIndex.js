9095a5aa3474af606918841377d41d7db8043ba3
Caleb9083 <calebosam12@gmail.com>
2023-01-03 09:35:06 +0000

    Add rendering of User specific categories

--- src/DashboardIndex.js
+++ src/DashboardIndex.js
@@ -2,8 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import DashboardMyDay from "./pages/Dashboard/DashboardMyDay";
+import DashboardWrapper from "./pages/Dashboard/DashboardWrapper";
 
 const DashboardIndex = () => {
+  const userCategories = ["Current month", "Last quarter", "Last year"];
+
+  let arr = [];
+  for (let i = 0; i < userCategories.length; i++) {
+    const userCategoryRoute = (
+      <Route
+        path={`/dashboard/${userCategories[i].replace(" ", "-").toLowerCase()}`}
+        element={
+          <DashboardWrapper>
+            <h1>{`${userCategories[i]}`}</h1>
+          </DashboardWrapper>
+        }
+      />
+    );
+    arr.push(userCategoryRoute);
+  }
+
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
@@ -12,6 +30,10 @@ const DashboardIndex = () => {
       <Route path="/dashboard/planned" element="" />
       <Route path="/dashboard/completed" element="" />
       <Route path="/dashboard/other" element="" />
+
+      {arr.map((el) => {
+        return el;
+      })}
     </Routes>
   );
 };
