b5dbb55188654abe9fe0f5ae1c66a32599efc4ad
Caleb9083 <calebosam12@gmail.com>
2023-01-03 11:29:42 +0000

    Create custom clock component

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import Clock from "react-clock";
+import "react-clock/dist/Clock.css";
+import Box from "@mui/material/Box";
+
+const ClockComponent = () => {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Box sx={{ mb: 1 }}>{new Date().toDateString()}</Box>
+      <Clock renderNumbers="true" size={250} value={value} />
+    </Box>
+  );
+};
+
+export default ClockComponent;
