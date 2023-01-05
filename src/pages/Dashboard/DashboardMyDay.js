83e3471674ad30a2b6dcfd5fa548a0f29edb0e9b
Caleb9083 <calebosam12@gmail.com>
2023-01-05 17:36:17 +0000

    Integrate My day page with backend

--- src/pages/Dashboard/DashboardMyDay.js
+++ src/pages/Dashboard/DashboardMyDay.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DashboardCategory from "../../components/DashboardCategory/DashboardCategory";
 import DashboardWrapper from "./DashboardWrapper";
+import { BASE_URL } from "../../config";
+import axios from "axios";
 
 const DashboardMyDay = () => {
+  const todayTodosUrl = `${BASE_URL}/api/v1/todos/todosByCategory/Today`;
+  const [todayTodos, setTodayTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(todayTodosUrl, {
+        headers: {
+          ContentType: "application/json",
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setTodayTodos(res.data.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }, []);
+
   return (
     <DashboardWrapper>
       <DashboardCategory
-        category="My Day"
+        category="Today"
         categoryDescription="Tasks for today"
-        todos={[
-          { name: "Learn Python" },
-          { name: "Learn Django", completed: true, important: true },
-        ]}
+        todos={todayTodos}
       />
     </DashboardWrapper>
   );
