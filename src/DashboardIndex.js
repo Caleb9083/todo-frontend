1ca296e93653316c70c64e106269e74071aa882c
Caleb9083 <calebosam12@gmail.com>
2023-01-05 22:44:12 +0000

    Refactor Dashboard index to use parameters

--- src/DashboardIndex.js
+++ src/DashboardIndex.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import DashboardCompleted from "./pages/Dashboard/DashboardCompleted";
-import DashboardImportant from "./pages/Dashboard/DashboardImportant";
-import DashboardMyDay from "./pages/Dashboard/DashboardMyDay";
-import DashboardOther from "./pages/Dashboard/DashboardOther";
-import DashboardPlanned from "./pages/Dashboard/DashboardPlanned";
 import DashboardWrapper from "./pages/Dashboard/DashboardWrapper";
 import DashboardCategory from "./components/DashboardCategory/DashboardCategory";
 
@@ -13,11 +8,6 @@ const DashboardIndex = () => {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dashboard/my-day" element={<DashboardMyDay />} />
-      <Route path="/dashboard/important" element={<DashboardImportant />} />
-      <Route path="/dashboard/planned" element={<DashboardPlanned />} />
-      <Route path="/dashboard/completed" element={<DashboardCompleted />} />
-      <Route path="/dashboard/other" element={<DashboardOther />} />
       <Route
         path="/dashboard/:category"
         element={
