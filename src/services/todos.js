7eb6d9ee3b2307affc0feec53dde30d3b654c3c1
Caleb9083 <calebosam12@gmail.com>
2023-01-06 16:31:34 +0000

    Create servive for get a todo

--- src/services/todos.js
+++ src/services/todos.js
@@ -20,6 +20,26 @@ const getTodosByCategory = async (category) => {
   }
 };
 
+const createTodo = async (data) => {
+  try {
+    const res = await API.post(`/todos`, data);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getTodo = async (todoId) => {
+  try {
+    const res = await API.get(`/todos/${todoId}`);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const todos = {
   getTodosByCategory,
+  createTodo,
+  getTodo,
 };
