0ec8a973796dcf4f51c98b865085fdeb9e30538e
Caleb9083 <calebosam12@gmail.com>
2023-01-16 19:17:58 +0000

    Remove user context

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import "./App.css";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavbarComponent from "../src/components/Navbar/NavbarComponent";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Home from "./pages/Home/Home";
 import DashboardIndex from "./DashboardIndex";
-import { UserContext } from "./context/userContext";
 
 function App() {
-  const [user, setUser] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    token: "",
-    isLoggedIn: "",
-  });
-
-  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
-
   return (
     <div className="App">
-      <UserContext.Provider value={value}>
-        <BrowserRouter>
-          <NavbarComponent />
-          <div className="pages">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sign-in" element={<SignIn />} />
-              <Route path="/sign-up" element={<SignUp />} />
-            </Routes>
-            <DashboardIndex />
-          </div>
-        </BrowserRouter>
-      </UserContext.Provider>
+      <BrowserRouter>
+        <NavbarComponent />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+          <DashboardIndex />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/context/userContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({
-  firstName: "",
-  lastName: "",
-  email: "",
-  token: "",
-  isLoggedIn: "",
-});
